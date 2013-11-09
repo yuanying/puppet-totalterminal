d3ef7ad45fd86e29c868c9e7517b339f2d918466
yuanying <yuanying@fraction.jp>
2013-11-09 10:52:21 +0900

    Update to 1.4.6.

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class totalterminal {
   package { 'TotalTerminal':
     provider => 'pkgdmg',
-    source   => 'http://downloads.binaryage.com/TotalTerminal-1.3.dmg'
+    source   => 'http://downloads.binaryage.com/TotalTerminal-1.4.6.dmg'
   }
 }
--- spec/classes/totalterminal_spec.rb
+++ spec/classes/totalterminal_spec.rb
@@ -4,7 +4,7 @@ describe 'totalterminal' do
   it do
     should contain_package('TotalTerminal').with({
       :provider => 'pkgdmg',
-      :source   => 'http://downloads.binaryage.com/TotalTerminal-1.3.dmg',
+      :source   => 'http://downloads.binaryage.com/TotalTerminal-1.4.6.dmg',
     })
   end
 end
